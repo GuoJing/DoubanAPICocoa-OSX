d53c31b7fec5dc3b79cdbe0c76bda25b90bc3496
GuoJing <soundbbg@gmail.com>
2012-11-12 17:57:17 +0800

    add file

---
+++ DoubanAPICocoa/DoubanAPICocoa.m
@@ -0,0 +1,13 @@
+//
+//  DoubanAPICocoa.m
+//  DoubanAPICocoa
+//
+//  Created by GuoJing on 12-11-12.
+//  Copyright (c) 2012年 GuoJing. All rights reserved.
+//
+
+#import "DoubanAPICocoa.h"
+
+@implementation DoubanAPICocoa
+
+@end
---
+++ DoubanAPICocoaTests/DoubanAPICocoaTests.h
@@ -0,0 +1,13 @@
+//
+//  DoubanAPICocoaTests.h
+//  DoubanAPICocoaTests
+//
+//  Created by GuoJing on 12-11-12.
+//  Copyright (c) 2012年 GuoJing. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DoubanAPICocoaTests : SenTestCase
+
+@end
