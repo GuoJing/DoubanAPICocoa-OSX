d224c23c907072b49687ce5cfce2947b9f277c13
GuoJing <soundbbg@gmail.com>
2012-12-20 10:56:35 +0800

    增加日记的engine

---
+++ DoubanAPICocoa/Src/Engine/Note/DOUNoteEngine.m
@@ -0,0 +1,13 @@
+//
+//  DOUNoteEngine.m
+//  DoubanAPICocoa
+//
+//  Created by GuoJing on 12-12-20.
+//  Copyright (c) 2012年 GuoJing. All rights reserved.
+//
+
+#import "DOUNoteEngine.h"
+
+@implementation DOUNoteEngine
+
+@end
